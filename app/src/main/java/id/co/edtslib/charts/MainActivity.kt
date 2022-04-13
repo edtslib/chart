31942c0ad6e92adb6c5e052fa4c04f8b857499ac
tonikustiana <toni.kustiana@sg-dsa.com>
2022-04-13 16:24:06 +0700

    fix PieChartView.kt

--- app/src/main/java/id/co/edtslib/charts/MainActivity.kt
+++ app/src/main/java/id/co/edtslib/charts/MainActivity.kt
@@ -2,14 +2,28 @@ package id.co.edtslib.charts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import id.co.edtslib.chart.PiChartListener
 import id.co.edtslib.chart.PieChartView
 
 class MainActivity : AppCompatActivity() {
+    private var toast: Toast? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val chartView = findViewById<PieChartView>(R.id.chartView)
-        chartView.setData(mutableListOf(10f, 40f, 50f))
+
+        val percents = mutableListOf(45f, 19f, 14f, 22f)
+        chartView.setPercent(percents)
+        chartView.listener = object : PiChartListener {
+            override fun onClick(index: Int) {
+                toast?.cancel()
+
+                toast = Toast.makeText(this@MainActivity, "abah ${percents[index]}", Toast.LENGTH_SHORT)
+                toast?.show()
+            }
+        }
     }
 }
--- chart/src/main/java/id/co/edtslib/chart/PieChartView.kt
+++ chart/src/main/java/id/co/edtslib/chart/PieChartView.kt
@@ -1,77 +1,191 @@
 package id.co.edtslib.chart
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
 import android.widget.FrameLayout
 
+
 open class PieChartView : FrameLayout {
+    var listener:  PiChartListener? = null
+    var showLabel = true
+    var labelColor = Color.WHITE
+    var labelSize = 0f
+    var borderWidth = 0f
+    var borderColor = Color.WHITE
+
     protected val paint = Paint()
-    private val startAngle = 0f
-    private var values: List<Float>? = null
+
+    private val startAngle = -90f
+    private var paths = mutableListOf<Path>()
+
+    private var percents: MutableList<Float>? = null
     protected open fun drawAddition(canvas: Canvas?) {}
 
     companion object {
-        val color = arrayListOf(Color.BLUE, Color.RED, Color.YELLOW)
+        val colors = arrayListOf(Color.parseColor("#9C9DA6"),
+            Color.parseColor("#1178D4"),
+            Color.parseColor("#6CA5E0"),
+            Color.parseColor("#FFA800"))
     }
 
     constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init(attrs)
+    }
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
-    )
+    ) {
+        init(attrs)
+    }
+
+    init {
+        isClickable = true
+    }
+
+    private fun init(attrs: AttributeSet?) {
+        if (attrs != null) {
+            val a = context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.PieChartView,
+                0, 0
+            )
+
+            showLabel = a.getBoolean(R.styleable.PieChartView_showLabel, true)
+            labelColor = a.getColor(R.styleable.PieChartView_labelColor, Color.WHITE)
+            labelSize = a.getDimension(R.styleable.PieChartView_labelSize, 0f)
+
+            borderWidth = a.getDimension(R.styleable.PieChartView_borderWidth, 0f)
+            borderColor = a.getColor(R.styleable.PieChartView_borderColor, Color.WHITE)
+
+            a.recycle()
+        }
+    }
 
     fun setData(values: List<Float>) {
-        this.values = values
+        percents = mutableListOf()
+
+        var total = 0f
+        for (f in values) {
+            total += f
+        }
+
+        var totalPct = 0f
+        for ((i, f) in values.withIndex()) {
+
+
+            if (i+1 == values.size) {
+                percents!!.add(100f-totalPct)
+            }
+            else {
+                val pct = f*100f/total
+                percents!!.add(pct)
+
+                totalPct += pct
+            }
+        }
+    }
+
+    fun setPercent(percents: List<Float>) {
+        this.percents = mutableListOf()
+        this.percents!!.addAll(percents)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when(event?.action) {
+            MotionEvent.ACTION_UP -> {
+                val x = event.x
+                val y = event.y
+
+                for ((i, path) in paths.withIndex()) {
+                    val rectF = RectF()
+                    path.computeBounds(rectF, true)
+                    if (x >= rectF.left && x <= rectF.right && y >= rectF.top && y <= rectF.bottom) {
+                        listener?.onClick(i)
+                    }
+                }
+            }
+        }
+        return super.onTouchEvent(event)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        drawX(canvas)
 
-        if (values?.isNotEmpty() == true) {
-            val total = getTotal()
-            if (total > 0) {
+    }
 
-                paint.style = Paint.Style.FILL_AND_STROKE
+    private fun drawX(canvas: Canvas?) {
+        paths.clear()
+        if (percents?.isNotEmpty() == true) {
 
-                var remain = 360f
-                var angle = startAngle
-                for ((i, item) in values!!.withIndex()) {
-                    val pct = item*360f/total
-                    val sweepAngle = if (i+1 == values!!.size) remain else pct
+            var angle = startAngle
+            val rect = Rect()
 
-                    remain -= pct
+            for ((i, item) in percents!!.withIndex()) {
+                val sweepAngle = item*360f/100.0f
 
-                    paint.color = color[i%color.size]
+                val path = Path()
+                path.addArc(RectF(0f, 0f, width.toFloat(), height.toFloat()), angle, sweepAngle)
+                path.lineTo(width/2.0f, height/2.0f)
 
-                    canvas?.drawArc(0f, 0f, width.toFloat(), height.toFloat(), angle, sweepAngle, true, paint)
-
-                    angle += pct
-                }
-
-
-
+                paths.add(path)
 
                 paint.style = Paint.Style.FILL
-                paint.color = Color.WHITE
+                paint.color = colors[i%colors.size]
+
+                canvas?.drawPath(path, paint)
+
+                if (borderWidth > 0f) {
+                    paint.style = Paint.Style.STROKE
+                    paint.strokeWidth = borderWidth
+                    paint.color = Color.WHITE
+                    canvas?.drawPath(path, paint)
+                }
+
+                if (showLabel) {
+                    if (labelSize == 0f) {
+                        labelSize = TypedValue.applyDimension(
+                            TypedValue.COMPLEX_UNIT_SP,
+                            17f, resources.displayMetrics
+                        )
+                    }
+
+                    paint.style = Paint.Style.FILL
+                    paint.textSize = labelSize
+                    paint.color = labelColor
+
+                    val bounds = RectF()
+                    path.computeBounds(bounds, false)
+                    val centerX: Float = bounds.centerX()
+                    val centerY: Float = bounds.centerY()
+
+                    val text = String.format("%.0f%%", item)
+                    paint.getTextBounds(text, 0, text.length, rect)
+
+                    //canvas?.drawText(String.format("%.0f%%", item), centerX, centerY, paint)
+                    canvas?.drawText(
+                        text,
+                        centerX - rect.width() / 2.0f,
+                        centerY + rect.height(),
+                        paint
+                    )
+                }
+
+                angle += sweepAngle
             }
+
+
+            paint.style = Paint.Style.FILL
+            paint.color = Color.WHITE
 
             drawAddition(canvas)
         }
-    }
-
-    private fun getTotal(): Float {
-        var total = 0.0f
-        if (values != null) {
-            for (item in values!!) {
-                total += item
-            }
-        }
-
-        return total
     }
 }
